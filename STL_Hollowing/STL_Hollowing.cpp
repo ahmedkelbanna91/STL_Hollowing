d7f48e9bc717d83e9dd8c400605370ca6f596d8b
Ahmed ElBanna <ahmedkelbanna91@gmail.com>
2024-05-19 15:28:42 +0200

    Update STL_Hollowing.cpp

--- STL_Hollowing/STL_Hollowing.cpp
+++ STL_Hollowing/STL_Hollowing.cpp
@@ -16,6 +16,7 @@
 #include <fstream>
 #include <iostream>
 
+
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef CGAL::Surface_mesh<Kernel::Point_3> Mesh;
 typedef Kernel::Point_3 Point;
